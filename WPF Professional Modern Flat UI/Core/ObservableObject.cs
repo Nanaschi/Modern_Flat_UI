95e02c53aa9487fe020ba9a5a38de183bfd805ab
Daniel <danil.e@ironwill-et.com>
2022-09-08 11:25:49 +0300

    Observable object and RelayCommand

---
+++ WPF Professional Modern Flat UI/Core/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using WPF_Professional_Modern_Flat_UI.Annotations;
+
+namespace WPF_Professional_Modern_Flat_UI.Core;
+
+public class ObservableObject: INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    [NotifyPropertyChangedInvocator]
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
---
+++ WPF Professional Modern Flat UI/Core/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPF_Professional_Modern_Flat_UI.Core;
+
+public class RelayCommand : ICommand
+{
+    private readonly Func<object, bool> _canExecute;
+    private readonly Action<object> _execute;
+
+    public bool CanExecute(object? parameter)
+    {
+        return _canExecute(null) || _canExecute(parameter);
+    }
+
+    public void Execute(object? parameter)
+    {
+        _canExecute(parameter);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    public RelayCommand(Func<object, bool> canExecute, Action<object> execute)
+    {
+        _canExecute = canExecute;
+        _execute = execute;
+    }
+}
